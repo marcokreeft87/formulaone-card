fb904be35f3214d42c1bbd7c9169873bdf125ddb
Marco Kreeft <marco.kreeft@gmail.com>
2022-10-05 11:05:07 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+const compressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+        filename: 'formulaone-card.js',
+        path: path.resolve(__dirname),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new compressionPlugin({
+            test: /\.js(\?.*)?$/i,
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }
+};
