25ad26e161faa99ff0c45e69f43e5b0b1a7cd195
Marco Kreeft <m.kreeft@dictu.nl>
2022-10-10 10:03:38 +0200

    -

---
+++ tests/utils/getCountryFlagUrl.test.ts
@@ -0,0 +1,14 @@
+import { getCountryFlagUrl } from '../../src/utils';
+
+describe('Testing util file function getCountryFlagUrl', () => {
+    test('Passing Japan should return expected flag url', () => {         
+        expect(getCountryFlagUrl('Japan')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-Japan.png')
+    }),
+    test('Passing USA should return expected flag url', () => {         
+        expect(getCountryFlagUrl('USA')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-United-States-of-America.png')
+    }),
+    test('Passing Saudi Arabia should return expected flag url', () => {         
+        expect(getCountryFlagUrl('Saudi-Arabia')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-Saudi-Arabia.png')
+    })
+})
+
