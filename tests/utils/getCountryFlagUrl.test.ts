38bb09bd8fc653cd010f1684b4bb7449b6a12bfc
Marco Kreeft <marco.kreeft@gmail.com>
2022-12-25 17:51:37 +0100

    Changed url for the flags to CDN (#37)
    * Changed url for the flags to CDN

--- tests/utils/getCountryFlagUrl.test.ts
+++ tests/utils/getCountryFlagUrl.test.ts
@@ -2,16 +2,15 @@ import { getCountryFlagUrl } from '../../src/utils';
 
 describe('Testing util file function getCountryFlagUrl', () => {
     test('Passing Japan should return expected flag url', () => {         
-        expect(getCountryFlagUrl('Japan')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-Japan.png')
+        expect(getCountryFlagUrl('Japan')).toBe('https://flagcdn.com/w40/japan.png')
     }),
     test('Passing USA should return expected flag url', () => {         
-        expect(getCountryFlagUrl('USA')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-United-States-of-America.png')
+        expect(getCountryFlagUrl('USA')).toBe('https://flagcdn.com/w40/us.png')
     }),
     test('Passing UAE should return expected flag url', () => {         
-        expect(getCountryFlagUrl('UAE')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-United-Arab-Emirates.png')
+        expect(getCountryFlagUrl('UAE')).toBe('https://flagcdn.com/w40/ae.png')
     }),
     test('Passing Saudi Arabia should return expected flag url', () => {         
-        expect(getCountryFlagUrl('Saudi-Arabia')).toBe('https://www.countries-ofthe-world.com/flags-normal/flag-of-Saudi-Arabia.png')
+        expect(getCountryFlagUrl('Saudi-Arabia')).toBe('https://flagcdn.com/w40/saudi-arabia.png')
     })
 })
-
