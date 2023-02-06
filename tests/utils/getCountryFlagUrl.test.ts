eaafd354ed1ff35eb2cc2bc404521953902e673c
Marco Kreeft <marco.kreeft@gmail.com>
2023-02-06 11:20:53 +0100

    Removed unused

--- tests/utils/getCountryFlagUrl.test.ts
+++ tests/utils/getCountryFlagUrl.test.ts
@@ -1,4 +1,4 @@
-import { getCountryFlagByName, getCountryFlagByNationality } from '../../src/utils';
+import { getCountryFlagByName } from '../../src/utils';
 import * as countries from '../testdata/countries.json';
 import { ImageConstants } from '../../src/lib/constants';
 import RestCountryClient from '../../src/api/restcountry-client';
@@ -17,19 +17,5 @@ describe('Testing util file function getCountryFlagUrl', () => {
     }),
     test('Passing UAE should return expected flag url', () => {         
         expect(getCountryFlagByName('UAE')).toBe(`${ImageConstants.FlagCDN}ae.png`)
-    })//,
-
-    // countries.forEach(country => {
-    //     if(country.Code == "HK") {
-    //         country.Code = "CN";
-    //     }
-
-    //     test(`Passing ${country.Nationality} to getCountryFlagByNationality should return correct flag url`, () => {
-    //         expect(getCountryFlagByNationality(country.Nationality)).toBe(`${ImageConstants.FlagCDN}${country.Code.toLowerCase()}.png`)
-    //     })
-
-    //     test(`Passing ${country.Country} to getCountryFlagByName should return correct flag url`, () => {
-    //         expect(getCountryFlagByName(country.Country)).toBe(`${ImageConstants.FlagCDN}${country.Code.toLowerCase()}.png`)
-    //     })
-    // });
+    })
 })
