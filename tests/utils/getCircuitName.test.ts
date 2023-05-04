4b08d015a22b0c876447793127cfe229400be669
Marco Kreeft <marco.kreeft@gmail.com>
2023-05-04 09:22:41 +0200

    Fixed track layout image of Miami (#232)

--- tests/utils/getCircuitName.test.ts
+++ tests/utils/getCircuitName.test.ts
@@ -2,10 +2,20 @@ import { getCircuitName } from '../../src/utils';
 
 describe('Testing util file function getCircuitName', () => {
     test('Passing Japan should return expected circuit name', () => {         
-        expect(getCircuitName('Japan')).toBe('Japan')
+        expect(getCircuitName({
+            country: 'Japan',
+            lat: '',
+            long: '',
+            locality: ''
+        })).toBe('Japan')
     }),
     test('Passing UAE should return expected circuit name', () => {         
-        expect(getCircuitName('UAE')).toBe('Abu_Dhabi')
+        expect(getCircuitName({
+            country: 'UAE',
+            lat: '',
+            long: '',
+            locality: ''
+        })).toBe('Abu_Dhabi')
     })
 })
 
